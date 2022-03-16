e1d3870a61a9b7a5d39703405444ce41a36fa0ba
lakindu hewawasam <lakindu2002@live.com>
2022-03-16 22:33:43 +0530

    adding the notes-table

--- core-infrastructure/dynamodb/note-table.ts
+++ core-infrastructure/dynamodb/note-table.ts
@@ -4,4 +4,13 @@ import * as aws from "@pulumi/aws";
 const config = new pulumi.Config(); // retrive the config information stored in the pulumi.STACK-NAME.yaml file
 const env = config.require('env'); // retrieve the env variable from the config file
 
-export const NotesTable = new aws.d
+export const notesTable = new aws.dynamodb.Table(`${env}-notes-table`, {
+    billingMode: 'PAY_PER_REQUEST',
+    attributes: [
+        {
+            name: 'id',
+            type: 'S'
+        }
+    ],
+    hashKey: 'id',
+});
