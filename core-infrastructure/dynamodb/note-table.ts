6bc092d4bfb8e6530fcb36dc36da54355159bf85
lakindu hewawasam <lakindu2002@live.com>
2022-03-16 22:32:47 +0530

    migrating directories

--- core-infrastructure/dynamodb/note-table.ts
+++ core-infrastructure/dynamodb/note-table.ts
@@ -3,3 +3,5 @@ import * as aws from "@pulumi/aws";
 
 const config = new pulumi.Config(); // retrive the config information stored in the pulumi.STACK-NAME.yaml file
 const env = config.require('env'); // retrieve the env variable from the config file
+
+export const NotesTable = new aws.d
