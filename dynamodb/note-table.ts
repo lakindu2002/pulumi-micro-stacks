92f51da200615dd7de936c21ae284ff158fa8209
lakindu hewawasam <lakindu2002@live.com>
2022-03-16 22:31:01 +0530

    initial commit + added dynamodb directory + updated dev env

---
+++ dynamodb/note-table.ts
@@ -0,0 +1,5 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+const config = new pulumi.Config(); // retrive the config information stored in the pulumi.STACK-NAME.yaml file
+const env = config.require('env'); // retrieve the env variable from the config file
